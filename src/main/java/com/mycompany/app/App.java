54d71ea2389735a01d9b1725454e8f4b2f706aec
Batuhancaglar1041 <batuhancaglar1041@gmail.com>
2024-03-12 17:01:34 +0300

    mustache is changed and app and test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -21,56 +21,64 @@ public class App
         get("/", (req, res) -> "Hello, World");
 
         post("/compute", (req, res) -> {
-            String inputArr1 = req.queryParams("inputParam1Arr");
-            java.util.Scanner sc1 = new java.util.Scanner(inputArr1);
-            sc1.useDelimiter("[;\r\n]+");
-            java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
-            while (sc1.hasNext()) {
-                int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
-                inputList.add(value);
-            }
+            //System.out.println(req.queryParams("input1"));
+            //System.out.println(req.queryParams("input2"));
 
-            String inputArr2 = req.queryParams("inputList2");
-            java.util.Scanner sc2 = new java.util.Scanner(inputArr2);
-            sc2.useDelimiter("[;\r\n]+");
+            String input1 = req.queryParams("input1");
+            java.util.Scanner sc1 = new java.util.Scanner(input1);
+            sc1.useDelimiter("[;\r\n]+");
+            java.util.ArrayList<Integer> inputList1 = new java.util.ArrayList<>();
+            while (sc1.hasNext())
+            {
+                int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+                inputList1.add(value);
+            }
+            System.out.println(inputList1);
+
+
+            String inputint1 = req.queryParams("input1").replaceAll("\\s","");
+            int input1AsInt = Integer.parseInt(inputint1);
+
+
+
+
+
+
+
+            String input2 = req.queryParams("input2");
+            java.util.Scanner sc2 = new java.util.Scanner(input2);
+            sc1.useDelimiter("[;\r\n]+");
             java.util.ArrayList<Integer> inputList2 = new java.util.ArrayList<>();
-            while (sc2.hasNext()) {
+            while (sc2.hasNext())
+            {
                 int value = Integer.parseInt(sc2.next().replaceAll("\\s",""));
                 inputList2.add(value);
             }
+            System.out.println(inputList2);
 
-            int[] inputParam1Arr = new int[inputList.size()];
-            for (int i = 0; i < inputParam1Arr.length; i++) {
-                inputParam1Arr[i] = inputList.get(i);
-            }
 
-            String input2 = req.queryParams("input2AsInt").replaceAll("\\s","");
-            if (input2.equals("")) // is empty
-                input2 = "0"; // set to zero
-            int input2AsInt = Integer.parseInt(input2);
+            String inputint2 = req.queryParams("input2").replaceAll("\\s","");
+            int input2AsInt = Integer.parseInt(inputint1);
 
-            String inputBool = req.queryParams("valid").replaceAll("\\s","");
-            if (inputBool.equals("")) // is empty
-                inputBool = "0"; // set to zero
 
-            int inputBoolAsInt = Integer.parseInt(inputBool);
+            int result = App.customMethod(inputList1,inputList2,input1AsInt ,input2AsInt);
 
-            boolean valid = false;
-            if (inputBoolAsInt == 1) valid = true;
-
-            double result = calculateAverage(inputList, inputParam1Arr);
-
-            Map<String, Object> map = new HashMap<>();
+            Map map = new HashMap();
             map.put("result", result);
             return new ModelAndView(map, "compute.mustache");
         }, new MustacheTemplateEngine());
 
-        get("/compute", (rq, rs) -> {
-            Map<String, Object> map = new HashMap<>();
-            map.put("result", "not computed yet!");
-            return new ModelAndView(map, "compute.mustache");
-        }, new MustacheTemplateEngine());
+
+        get("/compute",
+                (rq, rs) -> {
+                    Map map = new HashMap();
+                    map.put("result", "not computed yet!");
+                    return new ModelAndView(map, "compute.mustache");
+                },
+                new MustacheTemplateEngine());
     }
+
+
 
 
     static int getHerokuAssignedPort() {
@@ -80,23 +88,14 @@ public class App
         }
         return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
-
-    public static float calculateAverage(ArrayList<Integer> list, int[] array) {
-        // Listeden ve diziden alınan sayıların ortalamasını hesaplar
-        int sumList = 0;
-        for (int num : list) {
-            sumList += num;
-        }
-
-        int sumArray = 0;
-        for (int num : array) {
-            sumArray += num;
-        }
-
-        float average =  ( (sumList + sumArray) / (list.size() + array.length));
-
-        return average;
+    public static int customMethod( ArrayList<Integer> list1, ArrayList<Integer> list2,int num1, int num2) {
+        // Örnek bir işlem yapalım, iki sayının toplamını ve iki liste arasındaki eleman sayılarının çarpımını döndürelim
+        int sum = num1 + num2;
+        int product = list1.size() * list2.size();
+        return sum * product;
     }
+
+
 
 
     }
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,78 +1,60 @@
 package com.mycompany.app;
-import junit.framework.Test;
+import junit.framework.Assert;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
 import java.util.ArrayList;
-import java.util.Arrays;
-public class AppTest extends TestCase{
-    public AppTest(String testName) {
-        super(testName);
-    }
 
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
-    public void testCalculateAverageWithPositiveNumbers() {
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
+public class AppTest extends TestCase {
 
-        int[] array = {4, 5, 6};
-
-        float expectedAverage =  ((1 + 2 + 3 + 4 + 5 + 6) / 6);
-
-
-        assertEquals(expectedAverage, App.calculateAverage(list, array));
+    public void testCustomMethod_PositiveNumbers() {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(3);
+        list2.add(4);
+        int result = App.customMethod(list1, list2, 5, 6);
+        Assert.assertEquals(44, result);
     }
 
 
-    public void testCalculateAverageWithNegativeNumbers() {
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(-1);
-        list.add(-2);
-        list.add(-3);
-
-        int[] array = {-4, -5, -6};
-
-        float expectedAverage = -3.0F;
-
-        assertEquals(expectedAverage, App.calculateAverage(list, array));
+    public void testCustomMethod_NegativeNumbersAndEmptyLists() {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        int result = App.customMethod(list1, list2, -2, -3);
+        Assert.assertEquals(0, result);
     }
 
 
-    public void testCalculateAverageWithEmptyListAndNonEmptyArray() {
-        ArrayList<Integer> list = new ArrayList<>();
-        int[] array = {1, 2, 3};
-
-        double expectedAverage = (1 + 2 + 3) / 3.0; // Expected average is the sum of array elements divided by array size.
-
-        double actualAverage = App.calculateAverage(list, array);
-
-        assertEquals(expectedAverage, actualAverage, 0.0001); // Add a delta value for double comparison
-    }
-
-    public void testCalculateAverageWithEmptyList() {
-        ArrayList<Integer> list = new ArrayList<>();
-        int[] array = {1, 2, 3};
-
-        float expectedAverage = (float) ((1 + 2 + 3) / 3.0);
-
-        assertEquals(expectedAverage, App.calculateAverage(list, array));
+    public void testCustomMethod_SingleElementLists() {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(5);
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(6);
+        int result = App.customMethod(list1, list2, 1, 2);
+        Assert.assertEquals(3, result);
     }
 
 
-    public void testCalculateAverageWithEmptyArray() {
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
+    public void testCustomMethod_DifferentListLengthAndNumberTypes() {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(4);
+        list2.add(5);
+        int result = App.customMethod(list1, list2, 7, 8);
+        assertEquals(90, result);
+    }
 
-        int[] array = {};
 
-        float expectedAverage = (float) ((1 + 2 + 3) / 3.0);
 
-        assertEquals(expectedAverage, App.calculateAverage(list, array));
+    public void testCustomMethod_ZeroNumbersAndEmptyLists() {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(0);
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(0);
+        int result = App.customMethod(list1, list2, 0, 0);
+        Assert.assertEquals(0, result);
     }
 }
