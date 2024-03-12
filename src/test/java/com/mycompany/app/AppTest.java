a781994e0325bdf6cb3ac17a0a8b43a3392bc01b
Batuhancaglar1041 <batuhancaglar1041@gmail.com>
2024-03-12 14:13:06 +0300

    app java is changed

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -7,97 +7,73 @@ import com.mycompany.app.AppTest;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-public class AppTest
-        extends TestCase
-{
-    // following are variables that are to be used given to app as parameters
-    private int [] inpArr;
-    private ArrayList<Integer> inpArrayList;
-    private int threshold;
-    private boolean valid;
-
-    //following are expected results::
-    private int [] ExpectedinpArr;
-    private ArrayList<Integer> ExpectedinpArrayList;
-    private int Expectedthreshold;
-    private int Expectedvalid;
+import static junit.framework.Assert.assertEquals;
 
 
+import java.util.ArrayList;
 
+public class AppTest {
 
-    public AppTest( String testName )
-    {
+    void testCalculateAverageWithPositiveNumbers() {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
 
-        super( testName );
-    }
+        int[] array = {4, 5, 6};
 
-    public void testShouldReturnFalseWhenNotValid(){
-        inpArrayList= new ArrayList<Integer>(Arrays.asList(1,2,3,4));
-        inpArr= new int[]{10,1,2,3};
-        valid = false;
-        threshold = 2;
-        assertFalse(App.firstArraySumGreaterThanSecondArraySumAndAThresholdValueWhenBooleanValidIsTrue(inpArr,inpArrayList,threshold,valid));
+        double expectedAverage = (1 + 2 + 3 + 4 + 5 + 6) / 6.0;
 
-    }
-    public void testShouldReturnFalseWhenThresholdHigher(){
-        inpArrayList= new ArrayList<Integer>(Arrays.asList(1,2,3,4));
-        inpArr= new int[]{10,1,2,3};
-        valid = true;
-        threshold = 200;
-
-        assertFalse(App.firstArraySumGreaterThanSecondArraySumAndAThresholdValueWhenBooleanValidIsTrue(inpArr,inpArrayList,threshold,valid));
-
-    }
-    public void testShouldReturnTrue(){
-        inpArrayList= new ArrayList<Integer>(Arrays.asList(1,2,3,4));
-        inpArr= new int[]{10,1,2,3};
-        valid = true;
-        threshold = 10;
-
-        assertTrue(App.firstArraySumGreaterThanSecondArraySumAndAThresholdValueWhenBooleanValidIsTrue(inpArr,inpArrayList,threshold,valid));
-
-    }
-    public void testNull() {
-        inpArrayList= null;
-        inpArr = null;
-        valid = false;
-        threshold = 0;
-
-        assertFalse(App.firstArraySumGreaterThanSecondArraySumAndAThresholdValueWhenBooleanValidIsTrue(inpArr,inpArrayList,threshold,valid));
-
-    }
-    public void testWhenBothNotValidAndThresholdHigher(){
-        inpArrayList= new ArrayList<Integer>(Arrays.asList(1,2,3,4));
-        inpArr= new int[]{10,1,2,3};
-        valid = false;
-        threshold = 200;
-
-        assertFalse(App.firstArraySumGreaterThanSecondArraySumAndAThresholdValueWhenBooleanValidIsTrue(inpArr,inpArrayList,threshold,valid));
-
-
-    }
-    public void testWhenNotValidAndThresholdHigherAndSecondArrayHigher(){
-        inpArrayList= new ArrayList<Integer>(Arrays.asList(1,2,3,4,100));
-        inpArr= new int[]{10,1,2,3};
-        valid = false;
-        threshold = 200;
-
-        assertFalse(App.firstArraySumGreaterThanSecondArraySumAndAThresholdValueWhenBooleanValidIsTrue(inpArr,inpArrayList,threshold,valid));
-
-
-    }
-    public void testShouldReturnFalseSecondArrhigher(){
-        inpArrayList= new ArrayList<Integer>(Arrays.asList(1,2,3,4,250));
-        inpArr= new int[]{10,1,2,3};
-        valid = true;
-        threshold = 2;
-
-        assertFalse(App.firstArraySumGreaterThanSecondArraySumAndAThresholdValueWhenBooleanValidIsTrue(inpArr,inpArrayList,threshold,valid));
-    }
-
-    public static Test suite()
-    {  return new TestSuite( AppTest.class );
+        assertEquals(expectedAverage, App.calculateAverage(list, array));
     }
 
 
+    void testCalculateAverageWithNegativeNumbers() {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(-1);
+        list.add(-2);
+        list.add(-3);
+
+        int[] array = {-4, -5, -6};
+
+        double expectedAverage = (-1 - 2 - 3 - 4 - 5 - 6) / 6.0;
+
+        assertEquals(expectedAverage, App.calculateAverage(list, array));
+    }
+
+
+    void testCalculateAverageWithEmptyListAndArray() {
+        ArrayList<Integer> list = new ArrayList<>();
+        int[] array = {};
+
+        double expectedAverage = 0.0; // Since both list and array are empty, expected average is 0.
+
+        double actualAverage = App.calculateAverage(list, array);
+
+        assertEquals(expectedAverage, actualAverage);
+    }
+
+
+    void testCalculateAverageWithEmptyList() {
+        ArrayList<Integer> list = new ArrayList<>();
+        int[] array = {1, 2, 3};
+
+        double expectedAverage = (1 + 2 + 3) / 3.0;
+
+        assertEquals(expectedAverage, App.calculateAverage(list, array));
+    }
+
+
+    void testCalculateAverageWithEmptyArray() {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        int[] array = {};
+
+        double expectedAverage = (1 + 2 + 3) / 3.0;
+
+        assertEquals(expectedAverage, App.calculateAverage(list, array));
+    }
 }
